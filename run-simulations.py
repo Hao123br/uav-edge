e07cd22013cdf55950c5210c0d5b43345168f935
Hao123br <carlosh2340@gmail.com>
2022-07-08 14:08:51 -0300

    update sem script

--- run-simulations.py
+++ run-simulations.py
@@ -16,38 +16,41 @@ args = parser.parse_args()
 
 campaign = sem.CampaignManager.new(ns_path, script, campaign_dir,
             overwrite=args.overwrite, check_repo=True)
-print(campaign)
 
+print("running simulations without UAVs")
 param_combinations = {
     'seed' : 10000,
     'numSCs' : 1,
     'numUAVs' : 0,
     'numUEs' : [100, 200, 300],
-    'remMode' : 0
 }
 
-campaign.run_missing_simulations(sem.list_param_combinations(param_combinations),1)
+campaign.run_missing_simulations(sem.list_param_combinations(param_combinations),
+		runs=1, stop_on_errors=False)
 
 result_param = {
     'numSCs' : [1],
     'numUEs' : [100, 200, 300]
 }
 
+print("exporting results")
 campaign.save_to_folders(result_param, results_dir, 1)
 
+print("running simulations without UAVs")
 param_combinations = {
     'seed' : 10000,
     'numSCs' : 0,
     'numUAVs' : 8,
     'numUEs' : [100, 200, 300],
-    'remMode' : 0
 }
 
-campaign.run_missing_simulations(sem.list_param_combinations(param_combinations),1)
+campaign.run_missing_simulations(sem.list_param_combinations(param_combinations),
+		runs=2, stop_on_errors=False)
 
 result_param = {
     'numUAVs' : [8],
     'numUEs' : [100, 200, 300]
 }
 
-campaign.save_to_folders(result_param, results_dir, 1)
+print("exporting results")
+campaign.save_to_folders(result_param, results_dir, 2)

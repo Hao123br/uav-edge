aa8a231d37c1e3b9bdb5a750cb8774fcea6a9d26
Hao123br <carlosh2340@gmail.com>
2022-11-22 13:21:18 -0300

    plot script: change style to vertical bars with error bars

--- graph-scripts/qos-total.py
+++ graph-scripts/qos-total.py
@@ -56,50 +56,98 @@ def getData(scenarios):
 
 		if runs == []:
 			print("scenario " + scenario[1] + " has no data")
-			qos_mean = QosData(0,0,0)
+			qos_result = QosResult(0,0,0,0,0,0)
 		else:
 			print("scenario {} has {} data points".format(scenario[1], len(runs)))
-			qos_mean = QosData._make(np.mean(qos, axis=0))
+			qos_mean = np.mean(qos, axis=0)
+			qos_std = np.std(qos, axis=0)
+			qos_result = QosResult(
+				qos_mean[0], qos_std[0], 
+				qos_mean[1], qos_std[1],
+				qos_mean[2], qos_std[2]
+				)
 
-		data[scenario[0]] = qos_mean
+		data[scenario[0]] = qos_result
 	print("")
 	return data
 
-ScenarioVariant = namedtuple("ScenarioVariant", ["name", "path"])
+ScenarioVariant = namedtuple("ScenarioVariant", ["name", "path", "color"])
 QosData = namedtuple("QosData", ["pdr", "delay", "jitter"])
+QosResult = namedtuple("QosResult", ["pdr", "pdr_std", "delay", "delay_std", "jitter", "jitter_std"])
 
-scenario_variants = [ScenarioVariant("No UAVs", "../results/numSCs=1"),
-					 ScenarioVariant("With UAVs", "../results/numUAVs=8")]
+scenario_variants = [ScenarioVariant("No UAVs", "../results/numSCs=1", "#ffc0cb"),
+					 ScenarioVariant("With UAVs", "../results/numUAVs=8", "#c1e7ff")]
 
-fig = plt.figure(figsize=(13,3))
-ax_pdr = fig.add_subplot(131)
-ax_delay = fig.add_subplot(132)
-ax_jitter = fig.add_subplot(133)
+fig_pdr = plt.figure()
+ax_pdr = fig_pdr.subplots()
+
+fig_delay = plt.figure()
+ax_delay = fig_delay.subplots()
+
+fig_jitter = plt.figure()
+ax_jitter = fig_jitter.subplots()
+
+ind = np.arange(3)
+width = 0.15       # the width of the bars
+variantN = 0
+pdr_rectangles = []
+delay_rectangles = []
+jitter_rectangles = []
 
 for variant in scenario_variants:
 	scenarios = getScenarios(variant.path)
 	data_scenarios = getData(scenarios)
 
-	pdr_data = [data.pdr for data in data_scenarios.values()]
-	ax_pdr.plot(data_scenarios.keys(), pdr_data, label=variant.name)
-	ax_pdr.set_title("PDR")
-	ax_pdr.set(xticks=list(data_scenarios.keys()), xlabel="Number of users")
-	ax_pdr.set(ylim=[0,100], yticks=np.arange(0,101,10), ylabel="%")
-	ax_pdr.legend()
+	pdr_mean = [data.pdr for data in data_scenarios.values()]
+	pdr_std = [data.pdr_std for data in data_scenarios.values()]
+	rect = ax_pdr.bar(ind + variantN*width, pdr_mean, width, yerr=pdr_std, align='center', alpha=1.0, ecolor='black', capsize=6, color=variant.color, linewidth=1.0, edgecolor='black')
+	pdr_rectangles.append(rect[0])
+	ax_pdr.set(xticks=ind+width*2, xticklabels=list(data_scenarios.keys()))
+	ax_pdr.set_xlabel("Number of users", fontsize=17)
+	ax_pdr.tick_params(axis="x", labelsize=15)
+	ax_pdr.set_yticks(np.arange(0,101,10))
+	ax_pdr.set_ylabel("PDR (%)", fontsize=17)
+	ax_pdr.tick_params(axis="y", labelsize=15)
+	ax_pdr.yaxis.grid(color='gray', linestyle='dotted')
 
-	delay_data = [data.delay for data in data_scenarios.values()]
-	ax_delay.plot(data_scenarios.keys(), delay_data, label=variant.name)
-	ax_delay.set_title("Delay")
-	ax_delay.set(xticks=list(data_scenarios.keys()), xlabel="Number of users")
-	ax_delay.set(ylim=[0,120], yticks=np.arange(0,121,20), ylabel="milliseconds")
-	ax_delay.legend()
+	delay_mean = [data.delay for data in data_scenarios.values()]
+	delay_std = [data.delay_std for data in data_scenarios.values()]
+	rect = ax_delay.bar(ind + variantN*width, delay_mean, width, yerr=delay_std, align='center', alpha=1.0, ecolor='black', capsize=6, color=variant.color, linewidth=1.0, edgecolor='black')
+	delay_rectangles.append(rect[0])
+	ax_delay.set(xticks=ind+width*2, xticklabels=list(data_scenarios.keys()))
+	ax_delay.set_xlabel("Number of users", fontsize=17)
+	ax_delay.tick_params(axis="x", labelsize=15)
+	ax_delay.set_yticks(np.arange(0,121,10))
+	ax_delay.set_ylabel("Delay (ms)", fontsize=17)
+	ax_delay.tick_params(axis="y", labelsize=15)
+	ax_delay.yaxis.grid(color='gray', linestyle='dotted')
 
-	jitter_data = [data.jitter for data in data_scenarios.values()]
-	ax_jitter.plot(data_scenarios.keys(), jitter_data, label=variant.name)
-	ax_jitter.set_title("Jitter")
-	ax_jitter.set(xticks=list(data_scenarios.keys()), xlabel="Number of users")
-	ax_jitter.set(ylim=[0,20], yticks=np.arange(0,26,5), ylabel="milliseconds")
-	ax_jitter.legend()
+	jitter_mean = [data.jitter for data in data_scenarios.values()]
+	jitter_std = [data.jitter_std for data in data_scenarios.values()]
+	rect = ax_jitter.bar(ind + variantN*width, jitter_mean, width, yerr=jitter_std, align='center', alpha=1.0, ecolor='black', capsize=6, color=variant.color, linewidth=1.0, edgecolor='black')
+	jitter_rectangles.append(rect[0])
+	ax_jitter.set(xticks=ind+width*2, xticklabels=list(data_scenarios.keys()))
+	ax_jitter.set_xlabel("Number of users", fontsize=17)
+	ax_jitter.tick_params(axis="x", labelsize=15)
+	ax_jitter.set_yticks(np.arange(0,26,5))
+	ax_jitter.set_ylabel("Jitter (ms)", fontsize=17)
+	ax_jitter.tick_params(axis="y", labelsize=15)
+	ax_jitter.yaxis.grid(color='gray', linestyle='dotted')
 
-plt.savefig("qos.png", format='png', dpi=500, bbox_inches = "tight")
+	variantN += 1
+
+labels = [variant.name for variant in scenario_variants]
+ax_pdr.legend(pdr_rectangles, labels, loc='upper center', bbox_to_anchor=(0., 1.12, 0.95, .102),
+	    ncol=3, borderaxespad=0.,  prop={'size': 14})
+ax_delay.legend(delay_rectangles, labels, loc='upper center', bbox_to_anchor=(0., 1.12, 0.95, .102),
+	    ncol=3, borderaxespad=0.,  prop={'size': 14})
+ax_jitter.legend(jitter_rectangles, labels, loc='upper center', bbox_to_anchor=(0., 1.12, 0.95, .102),
+	    ncol=3, borderaxespad=0.,  prop={'size': 14})
+
+fig_pdr.savefig("pdr.png", format='png', dpi=300, bbox_inches = "tight")
+fig_pdr.savefig("pdr.pdf", format='pdf', dpi=300, bbox_inches = "tight")
+fig_delay.savefig("delay.png", format='png', dpi=300, bbox_inches = "tight")
+fig_delay.savefig("delay.pdf", format='pdf', dpi=300, bbox_inches = "tight")
+fig_jitter.savefig("jitter.png", format='png', dpi=300, bbox_inches = "tight")
+fig_jitter.savefig("jitter.pdf", format='pdf', dpi=300, bbox_inches = "tight")
 #plt.show()

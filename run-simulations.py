f969e784c66e33d02033a6a820dbc887aa2f4f30
Hao123br <carlosh2340@gmail.com>
2022-11-22 13:12:02 -0300

    update sem script

--- run-simulations.py
+++ run-simulations.py
@@ -5,9 +5,10 @@ import sem
 import argparse
 
 ns_path = './'
-script = 'simulation'
+script = 'ns3.36.1-simulation-optimized'
 campaign_dir = ns_path + 'sem'
 results_dir = ns_path + 'results'
+nRuns = 30
 
 parser = argparse.ArgumentParser(description='SEM script')
 parser.add_argument('-o', '--overwrite', action='store_true',
@@ -15,42 +16,36 @@ parser.add_argument('-o', '--overwrite', action='store_true',
 args = parser.parse_args()
 
 campaign = sem.CampaignManager.new(ns_path, script, campaign_dir,
-            overwrite=args.overwrite, check_repo=True)
+            overwrite=args.overwrite, check_repo=False)
 
-print("running simulations without UAVs")
+print("running simulations with eNBs")
 param_combinations = {
-    'seed' : 10000,
-    'numSCs' : 1,
-    'numUAVs' : 0,
-    'numUEs' : [100, 200, 300],
-}
-
-campaign.run_missing_simulations(sem.list_param_combinations(param_combinations),
-		runs=1, stop_on_errors=False)
-
-result_param = {
-    'numSCs' : [1],
-    'numUEs' : [100, 200, 300]
-}
-
-print("exporting results")
-campaign.save_to_folders(result_param, results_dir, 1)
-
-print("running simulations without UAVs")
-param_combinations = {
-    'seed' : 10000,
-    'numSCs' : 0,
+#	'RngSeed' : 4200,
+    'numENBs' : 1,
     'numUAVs' : 8,
+	'algo' : ['no-uavs', 'topsis'],
     'numUEs' : [100, 200, 300],
 }
 
 campaign.run_missing_simulations(sem.list_param_combinations(param_combinations),
-		runs=2, stop_on_errors=False)
+		runs=nRuns, stop_on_errors=False)
 
-result_param = {
-    'numUAVs' : [8],
+result_param = { 
+    'numENBs' : [1],
     'numUEs' : [100, 200, 300]
 }
 
 print("exporting results")
-campaign.save_to_folders(result_param, results_dir, 2)
+campaign.save_to_folders(result_param, results_dir, nRuns)
+
+print("running simulations without eNBs")
+param_combinations['numENBs'] = [0]
+param_combinations['algo'] = ['topsis']
+
+campaign.run_missing_simulations(sem.list_param_combinations(param_combinations),
+		runs=nRuns, stop_on_errors=False)
+
+result_param['numENBs'] = [0]
+
+print("exporting results")
+campaign.save_to_folders(result_param, results_dir, nRuns)

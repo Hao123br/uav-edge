2ec941495c61e4f8caeef92c302a68b9ca0a09fa
Hao123br <carlosh2340@gmail.com>
2022-11-22 13:26:38 -0300

    plot script: use pandas to handle the data

--- graph-scripts/qos-vs-users.py
+++ graph-scripts/qos-vs-users.py
@@ -3,6 +3,7 @@
 
 import math
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 import csv
 import glob
@@ -22,31 +23,16 @@ def getScenarios(path):
 
 
 def readFile(filePath):
-	pdr_all = []
-	delay_all = []
-	jitter_all = []
+	result = pd.read_csv(filePath, sep=',', names=['peers', 'pdr', 'plr', 'delay',
+	'jitter', 'throughput'], usecols=['pdr', 'delay', 'jitter'])
+	result = result.mean()
+	result['delay'] = result['delay'] * 1000
+	result['jitter'] = result['jitter'] * 1000
 
-	pdr_mean = 0
-	delay_mean = 0
-	jitter_mean = 0
-
-	with open(filePath) as data:
-		for peers, pdr, plr, delay, jitter, throughput in csv.reader(data,delimiter=','):
-			pdr_all.append(int(pdr))
-			if math.isnan(float(delay)):
-				delay_all.append(0)
-				jitter_all.append(0)
-			else:
-				delay_all.append(float(delay))
-				jitter_all.append(float(jitter))
-		pdr_mean = np.mean(pdr_all)
-		delay_mean = np.mean(delay_all) * 1000
-		jitter_mean = np.mean(jitter_all) * 1000
-
-	return QosData(pdr_mean, delay_mean, jitter_mean)
+	return result
 
 def getData(scenarios):
-	data = dict()
+	data = []
 	for scenario in scenarios.items():
 		runs = glob.glob(scenario[1] + "/*/qos.txt")
 		qos = []
@@ -56,24 +42,20 @@ def getData(scenarios):
 
 		if runs == []:
 			print("scenario " + scenario[1] + " has no data")
-			qos_result = QosResult(0,0,0,0,0,0)
+			zeroes = pd.Series(data=[0, 0, 0], index=['pdr', 'delay','jitter'])
+			d = {'mean' : zeroes, 'std' : zeroes}
 		else:
 			print("scenario {} has {} data points".format(scenario[1], len(runs)))
-			qos_mean = np.mean(qos, axis=0)
-			qos_std = np.std(qos, axis=0)
-			qos_result = QosResult(
-				qos_mean[0], qos_std[0], 
-				qos_mean[1], qos_std[1],
-				qos_mean[2], qos_std[2]
-				)
+			qos = pd.DataFrame(qos)
+			d = {'mean' : qos.mean(), 'std' : qos.std()}
+		qos_result = pd.DataFrame(d)
 
-		data[scenario[0]] = qos_result
+		data.append(qos_result)
+	data = pd.concat(data)
 	print("")
 	return data
 
 ScenarioVariant = namedtuple("ScenarioVariant", ["name", "path", "color"])
-QosData = namedtuple("QosData", ["pdr", "delay", "jitter"])
-QosResult = namedtuple("QosResult", ["pdr", "pdr_std", "delay", "delay_std", "jitter", "jitter_std"])
 
 scenario_variants = [ScenarioVariant("No UAVs", "../results/numSCs=1", "#ffc0cb"),
 					 ScenarioVariant("With UAVs", "../results/numUAVs=8", "#c1e7ff")]
@@ -98,11 +80,12 @@ for variant in scenario_variants:
 	scenarios = getScenarios(variant.path)
 	data_scenarios = getData(scenarios)
 
-	pdr_mean = [data.pdr for data in data_scenarios.values()]
-	pdr_std = [data.pdr_std for data in data_scenarios.values()]
+	pdr = data_scenarios.loc['pdr']
+	pdr_mean = pdr['mean']
+	pdr_std = pdr['std']
 	rect = ax_pdr.bar(ind + variantN*width, pdr_mean, width, yerr=pdr_std, align='center', alpha=1.0, ecolor='black', capsize=6, color=variant.color, linewidth=1.0, edgecolor='black')
 	pdr_rectangles.append(rect[0])
-	ax_pdr.set(xticks=ind+width*2, xticklabels=list(data_scenarios.keys()))
+	ax_pdr.set(xticks=ind+width*2, xticklabels=list(scenarios.keys()))
 	ax_pdr.set_xlabel("Number of users", fontsize=17)
 	ax_pdr.tick_params(axis="x", labelsize=15)
 	ax_pdr.set_yticks(np.arange(0,101,10))
@@ -110,11 +93,12 @@ for variant in scenario_variants:
 	ax_pdr.tick_params(axis="y", labelsize=15)
 	ax_pdr.yaxis.grid(color='gray', linestyle='dotted')
 
-	delay_mean = [data.delay for data in data_scenarios.values()]
-	delay_std = [data.delay_std for data in data_scenarios.values()]
+	delay = data_scenarios.loc['delay']
+	delay_mean = delay['mean']
+	delay_std = delay['std']
 	rect = ax_delay.bar(ind + variantN*width, delay_mean, width, yerr=delay_std, align='center', alpha=1.0, ecolor='black', capsize=6, color=variant.color, linewidth=1.0, edgecolor='black')
 	delay_rectangles.append(rect[0])
-	ax_delay.set(xticks=ind+width*2, xticklabels=list(data_scenarios.keys()))
+	ax_delay.set(xticks=ind+width*2, xticklabels=list(scenarios.keys()))
 	ax_delay.set_xlabel("Number of users", fontsize=17)
 	ax_delay.tick_params(axis="x", labelsize=15)
 	ax_delay.set_yticks(np.arange(0,121,10))
@@ -122,11 +106,12 @@ for variant in scenario_variants:
 	ax_delay.tick_params(axis="y", labelsize=15)
 	ax_delay.yaxis.grid(color='gray', linestyle='dotted')
 
-	jitter_mean = [data.jitter for data in data_scenarios.values()]
-	jitter_std = [data.jitter_std for data in data_scenarios.values()]
+	jitter = data_scenarios.loc['jitter']
+	jitter_mean = jitter['mean']
+	jitter_std = jitter['std']
 	rect = ax_jitter.bar(ind + variantN*width, jitter_mean, width, yerr=jitter_std, align='center', alpha=1.0, ecolor='black', capsize=6, color=variant.color, linewidth=1.0, edgecolor='black')
 	jitter_rectangles.append(rect[0])
-	ax_jitter.set(xticks=ind+width*2, xticklabels=list(data_scenarios.keys()))
+	ax_jitter.set(xticks=ind+width*2, xticklabels=list(scenarios.keys()))
 	ax_jitter.set_xlabel("Number of users", fontsize=17)
 	ax_jitter.tick_params(axis="x", labelsize=15)
 	ax_jitter.set_yticks(np.arange(0,26,5))
@@ -144,10 +129,10 @@ ax_delay.legend(delay_rectangles, labels, loc='upper center', bbox_to_anchor=(0.
 ax_jitter.legend(jitter_rectangles, labels, loc='upper center', bbox_to_anchor=(0., 1.12, 0.95, .102),
 	    ncol=3, borderaxespad=0.,  prop={'size': 14})
 
-fig_pdr.savefig("pdr.png", format='png', dpi=300, bbox_inches = "tight")
-fig_pdr.savefig("pdr.pdf", format='pdf', dpi=300, bbox_inches = "tight")
-fig_delay.savefig("delay.png", format='png', dpi=300, bbox_inches = "tight")
-fig_delay.savefig("delay.pdf", format='pdf', dpi=300, bbox_inches = "tight")
-fig_jitter.savefig("jitter.png", format='png', dpi=300, bbox_inches = "tight")
-fig_jitter.savefig("jitter.pdf", format='pdf', dpi=300, bbox_inches = "tight")
+fig_pdr.savefig("pdr-vs-users.png", format='png', dpi=300, bbox_inches = "tight")
+fig_pdr.savefig("pdr-vs-users.pdf", format='pdf', dpi=300, bbox_inches = "tight")
+fig_delay.savefig("delay-vs-users.png", format='png', dpi=300, bbox_inches = "tight")
+fig_delay.savefig("delay-vs-users.pdf", format='pdf', dpi=300, bbox_inches = "tight")
+fig_jitter.savefig("jitter-vs-users.png", format='png', dpi=300, bbox_inches = "tight")
+fig_jitter.savefig("jitter-vs-users.pdf", format='pdf', dpi=300, bbox_inches = "tight")
 #plt.show()
